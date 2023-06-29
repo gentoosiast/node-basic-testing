05c56343ab7799455e2763a86ed0a112049072a7
Sergey Koksharov <gentoosiastic@gmail.com>
2023-06-30 00:52:34 +0300

    feat: implement mocking-lib-api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,66 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
 
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const axiosCreateSpy = jest.spyOn(axios, 'create');
+    jest.spyOn(axios.Axios.prototype, 'get').mockResolvedValue({});
+
+    await throttledGetDataFromApi('todos');
+
+    jest.runAllTimers();
+
+    expect(axiosCreateSpy.mock.calls[0]?.[0]).toStrictEqual({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const relativeURL = 'todos';
+    const axiosGetSpy = jest
+      .spyOn(axios.Axios.prototype, 'get')
+      .mockResolvedValue({});
+
+    await throttledGetDataFromApi(relativeURL);
+
+    jest.runAllTimers();
+
+    expect(axiosGetSpy.mock.calls[0]?.[0]).toBe(relativeURL);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const relativeURL = 'todos';
+    const responseData = [
+      {
+        userId: 5,
+        id: 98,
+        title:
+          'debitis accusantium ut quo facilis nihil quis sapiente necessitatibus',
+        completed: true,
+      },
+      {
+        userId: 5,
+        id: 99,
+        title: 'neque voluptates ratione',
+        completed: false,
+      },
+    ];
+    jest
+      .spyOn(axios.Axios.prototype, 'get')
+      .mockResolvedValue({ data: responseData });
+
+    const result = await throttledGetDataFromApi(relativeURL);
+
+    jest.runAllTimers();
+
+    expect(result).toStrictEqual(responseData);
   });
 });
